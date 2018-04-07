a6e6067a13a41424da55c073c2251471e1a6e67f
Volodymyr Andriychak <andriychak@users.noreply.github.com>
2018-04-07 19:28:44 +0300

    initial commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class AuthService {
+
+  private registerUrl = "http://localhost:3000/api/register";
+  private loginUrl = "http://localhost:3000/api/login";
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(user) {
+    return this.http.post(this.registerUrl, user);
+  }
+
+  loginUser(user) {
+    return this.http.post(this.loginUrl, user);
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginUserData = {
+    email: "",
+    password: ""
+  }
+
+  private showError: boolean;
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.showError = false;
+  }
+
+  loginUser() {
+    this.auth.loginUser(this.loginUserData).subscribe(
+      res => {
+        console.log(res);
+        this.showError = false;
+        this.router.navigate(["/special"]);
+      },
+      err => { 
+        console.log(err);
+        this.showError = true;
+      });
+    }
+}
